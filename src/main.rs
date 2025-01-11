65ec4be37ccefab0e29a54d45938bad8bc21ea66
jeebuscrossaint <thediamond270@gmail.com>
2025-01-11 17:28:08 -0500

    hello world os panic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+mod vga_buffer;
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+    panic!("Some panic message");
+    loop {}
 }
