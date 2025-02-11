1aed5f450a56e76ed0f92eab4223ce95bb7c0e35
jeebuscrossaint <thediamond270@gmail.com>
2025-02-11 15:59:17 -0500

    ive made my mind :riscv 64

--- src/include/isr.h
+++
@@ -1,8 +0,0 @@
-#ifndef ISR_H
-#define ISR_H
-
-#include <stdint.h>
-
-extern void c_isr_handler(void);
-
-#endif // ISR_H
--- src/kernel/isr.c
+++
@@ -1,6 +0,0 @@
-#include <isr.h>
-#include <vga.h>
-
-void c_isr_handler(void) {
-    print("Interrupt received!\n", 0x0F);
-}
